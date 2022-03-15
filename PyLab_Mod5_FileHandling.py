cd950d8955f0c1a9324c6288aea7fd0cfe38eebd
Sara <sararghosalkar@gmail.com>
2022-03-15 20:42:37 +0530

    File Handling

---
+++ PyLab_Mod5_FileHandling.py
@@ -0,0 +1,38 @@
+#f = open("hellohehe.txt", "r")
+#print(f.read())
+
+#print(f.readline())
+#print(f.readline())
+#print(f.readline())
+
+#for x in f:
+    #print(x)
+
+#with open("pic.jpeg", 'rb') as f:
+    #contents = f.read()
+
+#f = open("hellohehe.txt", "a")
+#f.write(" JUST APPENDED THIS HAHA")
+#f = open("hellohehe.txt", "r")
+#print(f.read())
+#f.close()
+
+#f = open("hellohehe.txt", "w")
+#f.write("Woops! I have deleted the content!")
+#f.close()
+#try:
+    #f = open("file.txt", "x")
+#except FileExistsError :
+    #print("The file Exists")
+
+#import os
+#os.remove("myfile.txt")
+
+#import os
+#if os.path.exists("hellohehe.txt"):
+    #print("The file does exist")
+#else:
+    #print("The file does not exist")
+
+#import os
+#os.rmdir("untitled folder")
