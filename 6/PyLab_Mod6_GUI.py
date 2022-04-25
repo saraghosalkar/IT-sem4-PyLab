a0a71fce2892d527fd0d5a8504e60d5c7b38e66e
Sara <sararghosalkar@gmail.com>
2022-04-25 10:13:45 +0530

    New file

---
+++ 6/PyLab_Mod6_GUI.py
@@ -0,0 +1,7 @@
+import tkinter as tk
+from turtle import width
+window = tk.Tk()
+
+Label =tk.Label(window, text = "Hello", height= 10, width=20, fg="red",bg="orange")
+Label.grid()
+window.mainloop()
